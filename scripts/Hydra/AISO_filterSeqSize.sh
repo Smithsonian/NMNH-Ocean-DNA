9cfaccaf949ee12d66c557a0d378408756b6722f
Dan MacGuigan <daniel.macguigan@gmail.com>
2025-01-28 17:12:47 -0500

    added AISO_filterSeqSize.sh
    new script to filter all FASTA/Q sequence files in a directory by a minimum size threshold

---
+++ scripts/Hydra/AISO_filterSeqSize.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# ABOUT ################################################
+# author: Dan macguigan
+# contact: macguigand@si.edu
+#
+# Script to filter FASTA sequences by minumum size.
+# Will run on all FASTA files in the supplied directory
+# and write results to new files.
+# 
+# Written speificailly for NMNH Hydra cluster, but can
+# work on any computer with seqtk installed.
+# https://github.com/lh3/seqtk
+#
+
+# INPUTS ################################################
+
+# directory containing FASTA files you wish to filter by sequence size
+DIR="/pool/public/genomics/macguigand/test/files"
+
+# file suffix for your FASTA sequence files in DIR, must include the period
+SUFFIX=".fasta"
+
+# minumum sequence legth (in bp)
+MIN_L=1000
+
+# SCRIPT ################################################
+
+module load bioinformatics/seqtk/1.4
+
+mkdir ${DIR}/L${MIN_L}
+
+echo "Filtering files:"
+
+for file in "${DIR}"/*"${SUFFIX}"
+do
+  echo ${file}
+  base_name=$(basename ${file})
+  file_prefix=${base_name%"$SUFFIX"} # get base file name
+  seqtk seq -L "${MIN_L}" ${file} > ${DIR}/L${MIN_L}/${file_prefix}_L${MIN_L}${SUFFIX} # filter
+done
+
+# OUTPUT ################################################
+echo ""
+echo "Sequencing filtering complete!"
+echo "Results written to"
+echo "${DIR}/L${MIN_L}"
+echo "with file endings \"_L${MIN_L}${SUFFIX}\""
+
